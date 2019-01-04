1c9dacb8c6acc54f8113c71265fdab7789b64457
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2019-01-04 16:50:14 +0100

    feat: function isMultipleOf passing tests

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,5 +1,8 @@
 module.exports = {
     run: (number) => {
         return Array(number);
+    },
+    isMulipleOf: (base, number) => {
+        return number % base === 0;
     }
 }
--- test.js
+++ test.js
@@ -13,5 +13,28 @@ describe('FizzBuzz', () => {
         const list = fizzBuzz.run(listSize);
         expect(list.length).to.equal(listSize);
     });
+    
+    it('6 is multple of 3', () => {
+        const result = fizzBuzz.isMulipleOf(3,6);
+        expect(result).to.equal(true);
+    });
+
+    it('7 is not multple of 3', () => {
+        const result = fizzBuzz.isMulipleOf(3,7);
+        expect(result).to.equal(false);
+    });
+
+    it('10 is multiple of 5', () => {
+      const result = fizzBuzz.isMulipleOf(3,7);
+        expect(result).to.equal(false);
+    });
+
+    it('11 is not multiple of 5', () => {
+
+    });
+
+    it('15 is multiple of 15', () => {
+      
+    });        
 
 });
