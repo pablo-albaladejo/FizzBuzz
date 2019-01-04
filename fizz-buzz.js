9c4d05684f6e175c176ca80f9bf26c084bb929ad
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2019-01-04 17:42:49 +0100

    refactor: functions at fizzbuzz as const

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,12 +1,29 @@
-module.exports = {
-    run: (number) => {
-        let list = [];
-        for (i = 0; i < number; i++) {
-            list[i] = i + 1;
-        };
-        return list;
-    },
-    isMulipleOf: (base, number) => {
-        return number % base === 0;
+const run = (number) => {
+    let list = [];
+    for (i = 0; i < number; i++) {
+        list[i] = i + 1;
+    };
+    return list;
+}
+
+const isMulipleOf = (base, number) => {
+    return number % base === 0;
+}
+
+const getValue = (number) => {
+    if (isMulipleOf(number, 15)) {
+        return 'FizzBuzz'
+    } else if (isMulipleOf(number, 3)) {
+        return 'Fizz'
+    } else if (isMulipleOf(number, 5)) {
+        return 'Buzz'
+    } else {
+        return number;
     }
+}
+
+module.exports = {
+    getValue,
+    run,
+    isMulipleOf,
 }
