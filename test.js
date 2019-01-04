4e1cdc5c8090e62280239d6050730627250de094
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2019-01-04 16:34:15 +0100

    chore: basic project structure

---
+++ test.js
@@ -0,0 +1,18 @@
+const expect = require('expect.js');
+const fizzBuzz = require('./fizz-buzz.js');
+
+const listSize = 100;
+
+describe('FizzBuzz', () => {
+	
+	beforeEach(() => {
+
+	});
+
+	it('should return a list with proper size', () => {
+        const list = fizzBuzz(listSize);
+        
+		expect(list.length).to.equal(listSize);
+	});
+
+});
